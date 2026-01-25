c6466f511621b5aa0a8c920edcb43ca51ae23813
kongbanditsomsing-crypto <kongbanditsomsing@gmail.com>
2026-01-26 00:21:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -101,10 +101,10 @@ client.on('interactionCreate', async interaction => {
 }
 
 if (mode === 'custom') {
-  return interaction.reply({
-    content: `พิมพ์เนื้อหาที่จะส่งให้**${user}**`,
-    ephemeral: true
-  });
+   return interaction.reply({
+      content: `พิมพ์เนื้อหาที่จะส่งให้**${user}**`,
+      ephemeral: true
+   });
 }
 
 } // ปิด interaction.isButton()
