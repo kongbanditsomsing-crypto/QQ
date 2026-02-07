a101ef64b922768a6c4302b1835a3a7f04ed5592
kongbanditsomsing-crypto <kongbanditsomsing@gmail.com>
2026-02-07 12:06:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -389,7 +389,6 @@ client.on("interactionCreate", async (interaction) => {
       }
       }
       }
-      );
 
 client.on("interactionCreate", async (interaction) => {
   if (interaction.commandName === "create_room") {
