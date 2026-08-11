f6803d7eead9ba881b93844b6cea222c66979e75
kongbanditsomsing-crypto <kongbanditsomsing@gmail.com>
2026-08-11 23:30:53 +0700

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot Alive! 24/7"
+
+def run():
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    Thread(target=run).start()
